c222efb0e34fccbd57020ff99c7da6d76b9ab772
Damon Bogich <damonbogich@gmail.com>
2021-02-01 10:27:23 -0700

    setup on my branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import UserCard from './Components/UserCard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myData: {},
+      friends: []
+    };
+  }
+  // componentDidMount() {
+  //   axios.get('')
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>yo</h1>
+        <UserCard/>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
---
+++ src/Components/UserCard.js
@@ -0,0 +1,7 @@
+export default function UserCard() {
+    return (
+        <div>
+            <h1>usercard Component</h1>
+        </div>
+    );
+};
